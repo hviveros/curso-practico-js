a1e5383aff0f19988db7c3f92db489157400c014
Humberto Viveros <humberto.viverosrios@gmail.com>
2021-08-04 23:48:46 -0400

    Calcular Mediana, ordenando la lista

--- mediana.js
+++ mediana.js
@@ -3,37 +3,47 @@ function calcularMediaAritmetica(lista) {
         function(valorAcumulado = 0, nuevoElemento) {
             return valorAcumulado + nuevoElemento;
         }
-    );
-    
+    );    
     const promedioLista = sumaLista / lista.length;
-
     return promedioLista;
 }
 
-const lista1 = [
-    100, 200, 500, 400000000
-];
+// const lista1 = [
+//     100, 200, 500, 400000000
+// ];
 
-const mitadLista1 = parseInt(lista1.length / 2) ;
-
-function esPar(numerito){
-    if( numerito % 2 === 0){
-        return true;
-    } else {
-        return false;
-    }
+function ordenarLista(lista){
+    const list = lista.sort();
+    return list;
 }
 
-let mediana;
+function calcularMediana(lista1){
 
-if (esPar(lista1.length)) {
-    const elemento1 = lista1[mitadLista1 - 1];
-    const elemento2 = lista1[mitadLista1];
+    const listaOrdenada = ordenarLista(lista1);
 
-    const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+    const mitadlistaOrdenada = parseInt(listaOrdenada.length / 2) ;
 
-    mediana = promedioElemento1y2;
+    function esPar(numerito){
+        if( numerito % 2 === 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-} else {
-    mediana = lista1[mitadLista1];
+    let mediana;
+
+    if (esPar(listaOrdenada.length)) {
+        const elemento1 = listaOrdenada[mitadlistaOrdenada - 1];
+        const elemento2 = listaOrdenada[mitadlistaOrdenada];
+
+        const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+
+        mediana = promedioElemento1y2;
+
+    } else {
+        mediana = listaOrdenada[mitadlistaOrdenada];
+    }
+
+    return mediana;
 }
